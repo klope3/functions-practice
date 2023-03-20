9bbca0085a134432087e34cdde2d1e2913537429
Josh Klope <klope3@msn.com>
2023-03-20 14:28:22 -0500

    Solve

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,10 +9,11 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
+// function splitFirstAndLastNames(str) {
+//   return str.split(" ");
+// }
 
+const splitFirstAndLastNames = (str) => str.split(" ");
 
 /** =========================
  * The personAge() constant is assigned to a function, that takes an object of person Data as the argument.
@@ -20,11 +21,11 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+// const personAge = function(personObject) {
+//   return personObject.age;
+// }
 
-
+const personAge = (personObject) => personObject.age;
 /** =========================
  * The isNameInArray function takes an array and string of name as the arguments.
  * The function returns the boolean (true/false) if the name exists in the given array
@@ -32,10 +33,11 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
+// const isNameInArray = function(arr, name) {
+//   return arr.includes(name);
+// }
 
+const isNameInArray = (arr, name) => arr.includes(name);
 
 /** =========================
  * The logTimer function takes a number as the arguments.
@@ -44,24 +46,32 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+// const logSecondsUpToMax = function(max) {
+//   let i = 0;
+//   const timer = setInterval(function() {
+//     if (i < max) {
+//       console.log(++i);
+//     } else {
+//       clearInterval(timer);
+//     }
+//   }, 1000);
+// }
+
+const logSecondsUpToMax = (max) => {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(() => {
     if (i < max) {
       console.log(++i);
     } else {
       clearInterval(timer);
     }
   }, 1000);
-}
-
-
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
-
 
 // Do not delete or change the lines beneath
 const f = {
@@ -69,5 +79,5 @@ const f = {
   personAge: personAge || undefined,
   isNameInArray: isNameInArray || undefined,
   logSecondsUpToMax: logSecondsUpToMax || undefined,
-}
-export { f }
+};
+export { f };
--- exercises/e5.js
+++ exercises/e5.js
@@ -1,8 +1,7 @@
-// In this exercise you'll complete a series of "comparison" mini exercises. 
-// Conditional logic is a huge part of coding. It's what helps a program make decisions. 
+// In this exercise you'll complete a series of "comparison" mini exercises.
+// Conditional logic is a huge part of coding. It's what helps a program make decisions.
 // Finish each exercise below.
 // You can use ANY type of function syntax you want: named, arrow, or expression with anonymous function.
-
 
 /** =========================
  * Create a getDiffTwentySeven(num) function to return the difference between a given number and 27
@@ -13,8 +12,10 @@
 
 // Your code goes here...
 
-
-
+function getDiffTwentySeven(num) {
+  if (num <= 27) return 27 - num;
+  else return Math.abs(27 - num) * 2;
+}
 
 /** =========================
  * Create a sumOfTwoIntegers(num1, num2) function to compute and return the sum of the two given integers
@@ -25,8 +26,10 @@
 
 // Your code goes here
 
-
-
+function sumOfTwoIntegers(num1, num2) {
+  if (num1 !== num2) return num1 + num2;
+  else return (num1 + num2) * 3;
+}
 
 /** =========================
  * Create a isOneOfThemOrTheirSumIsFourty(num1, num2) function to check two given numbers and return a boolean.
@@ -39,19 +42,19 @@
 
 // Your code goes here...
 
-
-
+function isOneOfThemOrTheirSumIsFourty(num1, num2) {
+  return num1 === 40 || num2 === 40 || num1 + num2 === 40;
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-5"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
 
-
 // Do not delete or change the lines beneath
 const f = {
   getDiffTwentySeven: getDiffTwentySeven || undefined,
   sumOfTwoIntegers: sumOfTwoIntegers || undefined,
   isOneOfThemOrTheirSumIsFourty: isOneOfThemOrTheirSumIsFourty || undefined,
-}
-export { f }
+};
+export { f };
